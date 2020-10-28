80457908124a75fa6485c733e6fbd21c22bb3951
LeviathanZC <leviathan.zercomp@gmail.com>
2020-10-29 01:57:53 +0300

    Create superclass for domain entities.

--- src/main/java/by/zercomp/application/model/entity/Identifiable.java
+++ src/main/java/by/zercomp/application/model/entity/Identifiable.java
@@ -4,6 +4,40 @@ import java.io.Serializable;
 
 public class Identifiable implements Serializable {
 
+    private Long id;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Identifiable that = (Identifiable) o;
+
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Identifiable{");
+        sb.append("id=").append(id);
+        sb.append('}');
+        return sb.toString();
+    }
 }
