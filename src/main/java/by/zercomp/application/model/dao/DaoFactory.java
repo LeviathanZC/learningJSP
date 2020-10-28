2944c6ecf0d099cb41899b9cfb5f8e914f815201
LeviathanZC <leviathan.zercomp@gmail.com>
2020-10-29 01:47:28 +0300

    Create pattern for DaoFactory

--- src/main/java/by/zercomp/application/model/dao/DaoFactory.java
+++ src/main/java/by/zercomp/application/model/dao/DaoFactory.java
@@ -2,4 +2,18 @@ package by.zercomp.application.model.dao;
 
 public final class DaoFactory {
 
+    private static final DaoFactory instance = new DaoFactory();
+    //ENUMERATION OF DAO INSTANCES HERE...
+    //private final ___Dao ___Dao = new ___Dao();
+
+    private DaoFactory() {
+    }
+
+    /*
+     public Dao get___Dao() {
+        return ___Dao;
+     }
+     */
+
+
 }
