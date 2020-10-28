ec927ecb20b32812271b3250608c360f6742ef46
LeviathanZC <leviathan.zercomp@gmail.com>
2020-10-29 02:15:40 +0300

    Add equals and hashCode to User.java

--- src/main/java/by/zercomp/application/model/entity/User.java
+++ src/main/java/by/zercomp/application/model/entity/User.java
@@ -68,4 +68,38 @@ public class User extends Identifiable {
     public void setBirth(LocalDate birth) {
         this.birth = birth;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) return false;
+
+        User user = (User) o;
+        if (getRole() != user.getRole()) return false;
+        if (getLogin() != null ? !getLogin().equals(user.getLogin()) : user.getLogin() != null) return false;
+        if (getEmail() != null ? !getEmail().equals(user.getEmail()) : user.getEmail() != null) return false;
+        if (getFirstName() != null ? !getFirstName().equals(user.getFirstName()) : user.getFirstName() != null)
+            return false;
+        if (getLastName() != null ? !getLastName().equals(user.getLastName()) : user.getLastName() != null)
+            return false;
+        return getBirth() != null ? getBirth().equals(user.getBirth()) : user.getBirth() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 113;
+        int result = super.hashCode();
+        result = prime * result + (getRole() != null ? getRole().hashCode() : 0);
+        result = prime * result + (getLogin() != null ? getLogin().hashCode() : 0);
+        result = prime * result + (getEmail() != null ? getEmail().hashCode() : 0);
+        result = prime * result + (getFirstName() != null ? getFirstName().hashCode() : 0);
+        result = prime * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = prime * result + (getBirth() != null ? getBirth().hashCode() : 0);
+        return result;
+    }
 }
