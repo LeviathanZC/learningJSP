f5b52df3838b5c5def01f61550be46ecd6553e09
LeviathanZC <leviathan.zercomp@gmail.com>
2020-10-29 02:10:28 +0300

    Add getters/setters to User.java

--- src/main/java/by/zercomp/application/model/entity/User.java
+++ src/main/java/by/zercomp/application/model/entity/User.java
@@ -21,5 +21,51 @@ public class User extends Identifiable {
         this.birth = birth;
     }
 
+    public Role getRole() {
+        return role;
+    }
 
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
 }
