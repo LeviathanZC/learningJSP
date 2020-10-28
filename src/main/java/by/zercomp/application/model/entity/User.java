6bdc5170b17c21994b3dc00d8200a33c9cc16924
LeviathanZC <leviathan.zercomp@gmail.com>
2020-10-29 02:09:41 +0300

    Create user ctr

--- src/main/java/by/zercomp/application/model/entity/User.java
+++ src/main/java/by/zercomp/application/model/entity/User.java
@@ -1,12 +1,25 @@
 package by.zercomp.application.model.entity;
 
-public class User {
+import java.time.LocalDate;
+
+public class User extends Identifiable {
 
     private Role role;
     private String login;
     private String email;
     private String firstName;
     private String lastName;
+    private LocalDate birth;
+
+    public User() {}
+
+    public User(String login, String email, String firstName, String lastName, LocalDate birth) {
+        this.login = login;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birth = birth;
+    }
 
 
 }
