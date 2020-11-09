33b24cedc992f2a72055267fa149a4b4e7911fd5
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-10 02:19:20 +0300

    Create entity : Brand

---
+++ src/main/java/by/zercomp/application/model/entity/Brand.java
@@ -0,0 +1,51 @@
+package by.zercomp.application.model.entity;
+
+public class Brand extends Identifiable {
+
+    private String name;
+
+    public Brand() {
+    }
+
+    public Brand(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) return false;
+
+        Brand brand = (Brand) o;
+
+        return getName() != null ? getName().equals(brand.getName()) : brand.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 113;
+        int result = super.hashCode();
+        result = prime * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder("Brand{");
+        builder.append("name='").append(this.name).append("'\n}");
+        return builder.toString();
+    }
+}
