88dc62fc254e8f7d36bb075424b35a1494afcf6b
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-12 00:43:04 +0300

    Create : ConnectionPoolException

---
+++ src/main/java/by/zercomp/application/model/exception/ConnectionPoolException.java
@@ -0,0 +1,20 @@
+package by.zercomp.application.model.exception;
+
+public class ConnectionPoolException extends RuntimeException {
+    public ConnectionPoolException() {
+        super();
+    }
+
+    public ConnectionPoolException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConnectionPoolException(Throwable cause) {
+        super(cause);
+    }
+
+}
