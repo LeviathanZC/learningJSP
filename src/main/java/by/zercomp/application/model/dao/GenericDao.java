f269d626b59c2677c707adf9f3d410ba08210452
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-12 00:48:52 +0300

    Add : startTransaction() method to GenericDao

--- src/main/java/by/zercomp/application/model/dao/GenericDao.java
+++ src/main/java/by/zercomp/application/model/dao/GenericDao.java
@@ -1,14 +1,25 @@
 package by.zercomp.application.model.dao;
 
 import by.zercomp.application.model.entity.Identifiable;
+import by.zercomp.application.model.pool.ConnectionPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class GenericDao<T extends Identifiable> {
     private static final Logger log = LogManager.getLogger(GenericDao.class);
+    protected static final ConnectionPool pool = ConnectionPool.getInstance();
 
     protected GenericDao() {
     }
+
+    protected void startTransaction(Connection connection) throws SQLException {
+        connection.setAutoCommit(false);
+        connection.setTransactionIsolation(connection.TRANSACTION_SERIALIZABLE);
+    }
+
 
 
 }
