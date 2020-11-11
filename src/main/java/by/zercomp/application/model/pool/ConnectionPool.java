fea6af51ce777dd557990020b5ebedaef3f301bc
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-11 11:16:55 +0300

    Create : mysqlConnection.properties & setting up ConnectionPool

---
+++ src/main/java/by/zercomp/application/model/pool/ConnectionPool.java
@@ -0,0 +1,50 @@
+package by.zercomp.application.model.pool;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+
+public class ConnectionPool {
+    public static final String CONNECTION_PROPERTIES_FILE = "mysqlConnection.properties";
+    public static final String URL = "jdbc:mysql://127.0.0.1:3306/online_store";
+    public static final int DEFAULT_POOL_SIZE = 32;
+    private final BlockingQueue<Connection> freeConnection;
+    private final Queue<Connection> givenAwayConnections;
+
+    private final Logger log = LogManager.getLogger(ConnectionPool.class);
+
+
+    /*public static void main(String[] args) {
+        Properties prop = new Properties();
+        prop.put("user", "root");
+        prop.put("password", "pass");
+        prop.put("autoReconnect", "true");
+        prop.put("characterEncoding", "UTF-8");
+        prop.put("useUnicode", "true");
+
+        Connection cn = null;
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            cn = DriverManager.getConnection(URL, "leviathan", "1591310486+");
+            try (Statement st = cn.createStatement()) {
+                ResultSet rs = st.executeQuery("SELECT email, pw_hash FROM users;");
+                System.out.println(rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (cn != null) {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }*/
+}
