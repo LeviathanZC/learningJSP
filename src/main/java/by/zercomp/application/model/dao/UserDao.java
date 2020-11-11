7d9e616294f8f884c40ae88bc868cf53005e3efe
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-12 01:26:21 +0300

    Create : UserDao iface

---
+++ src/main/java/by/zercomp/application/model/dao/UserDao.java
@@ -0,0 +1,20 @@
+package by.zercomp.application.model.dao;
+
+import by.zercomp.application.model.entity.User;
+import by.zercomp.application.model.exception.DaoException;
+
+import java.util.Optional;
+
+public interface UserDao {
+    void create(User user, String pwHash) throws DaoException;
+
+    Optional<User> findById(Long id) throws DaoException;
+
+    Optional<User> findByLogin(String login) throws DaoException;
+
+    Optional<User> findByEmail(String email) throws DaoException;
+
+    Optional<String> findPasswordByLogin(String login) throws DaoException;
+
+    void updatePassword(String email, String newPassword) throws DaoException;
+}
