de0e120e9c1838fd40150b8bec8c2777ed6f3204
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-12 00:24:20 +0300

    Impl : close() method at ProxyConnection from Connection

--- src/main/java/by/zercomp/application/model/pool/ProxyConnection.java
+++ src/main/java/by/zercomp/application/model/pool/ProxyConnection.java
@@ -22,4 +22,8 @@ public class ProxyConnection implements Connection {
         }
     }
 
+    @Override
+    public void close() throws SQLException {
+        ConnectionPool.getInstance().releaseConnection(this);
+    }
 }
