61ffd088cd9803104002e308643ff3878c0f9288
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-15 13:04:44 +0300

    Start fill ColumnNames

--- src/main/java/by/zercomp/application/model/dao/ColumnName.java
+++ src/main/java/by/zercomp/application/model/dao/ColumnName.java
@@ -1,5 +1,32 @@
 package by.zercomp.application.model.dao;
 
 public class ColumnName {
+    //Users
+    public static String ID = "id";
+    public static String LOGIN = "login";
+    public static String EMAIL = "email";
+    public static String PASSWORD = "pw_hash";
+    public static String ROLE = "role_code";
+    public static String FIRST_NAME = "name";
+    public static String LAST_NAME = "surname";
+    public static String BIRTH_DATE = "birthDate";
 
+    //User roles
+    public static String ROLE_ID = "group_id";
+    public static String ROLE_NAME = "name";
+
+    //Orders
+    public static String ORDER_ID = "order_id";
+    public static String ORDER_USER_ID = "user_id";
+    public static String ORDER_DATE = "order_date";
+    public static String ORDER_STATUS = "status_code";
+    public static String ORDER_DELIVERY = "delivery_id";
+
+    //Order status
+    public static String STATUS_CODE = "status_code";
+    public static String STATUS_NAME = "status_name";
+
+    //Deliveries
+    public static String DELIVERY_ID = "id_delivery";
+    public static String STUB = "id_delivery";
 }
