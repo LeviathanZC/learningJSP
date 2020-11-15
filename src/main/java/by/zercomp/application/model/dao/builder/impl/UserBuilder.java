5e1ee5224993dfa1fb84b42a1ec01e44012613dc
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-15 13:28:48 +0300

    Impl : finish with UserBuilder

--- src/main/java/by/zercomp/application/model/dao/builder/impl/UserBuilder.java
+++ src/main/java/by/zercomp/application/model/dao/builder/impl/UserBuilder.java
@@ -2,16 +2,24 @@ package by.zercomp.application.model.dao.builder.impl;
 
 import by.zercomp.application.model.dao.ColumnName;
 import by.zercomp.application.model.dao.builder.GeneralBuilder;
+import by.zercomp.application.model.entity.Role;
 import by.zercomp.application.model.entity.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class UserBuilder implements GeneralBuilder<User> {
     @Override
     public User build(ResultSet resultSet) throws SQLException {
         User user = new User();
-        Long id = resultSet.getLong(ColumnName.USER_ID);
-
+        user.setId(resultSet.getLong(ColumnName.USER_ID));
+        user.setRole(Role.valueOf(resultSet.getString(ColumnName.ROLE_NAME).toUpperCase()));
+        user.setLogin(resultSet.getString(ColumnName.LOGIN));
+        user.setEmail(resultSet.getString(ColumnName.EMAIL));
+        user.setFirstName(resultSet.getString(ColumnName.FIRST_NAME));
+        user.setLastName(resultSet.getString(ColumnName.LAST_NAME));
+        user.setBirth(LocalDate.parse(resultSet.getDate(ColumnName.BIRTH_DATE).toString()));
+        return user;
     }
 }
