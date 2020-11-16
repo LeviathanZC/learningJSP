5c231113a827e3ea028960c641a945b386af67f2
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-16 15:14:01 +0300

    Create : UserService iface

--- src/main/java/by/zercomp/application/model/service/UserService.java
+++ src/main/java/by/zercomp/application/model/service/UserService.java
@@ -1,7 +1,22 @@
 package by.zercomp.application.model.service;
 
+import by.zercomp.application.model.entity.User;
+import by.zercomp.application.model.exception.ServiceException;
+
+import java.util.Map;
 import java.util.Optional;
 
 public interface UserService {
+
     Optional<String> signUp(User user, String password) throws ServiceException;
+
+    Optional<User> signIn(String login, String password) throws ServiceException;
+
+    Optional<User> findById(long id) throws ServiceException;
+
+    Optional<User> findByLogin(String login) throws ServiceException;
+
+    Optional<User> findByEmail(String email) throws ServiceException;
+
+    boolean updatePassword(Map<String, String> changeData) throws ServiceException;
 }
