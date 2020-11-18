10be1fc64514d85aa1f28d6489334e2871d41423
LeviathanZC <leviathan.zercomp@gmail.com>
2020-11-18 14:07:23 +0300

    Create : Router

---
+++ src/main/java/by/zercomp/application/controller/router/Router.java
@@ -0,0 +1,21 @@
+package by.zercomp.application.controller.router;
+
+public class Router {
+    private String page;
+
+    public Router() {}
+
+    public Router(String page) {
+        this.page = page;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+
+}
