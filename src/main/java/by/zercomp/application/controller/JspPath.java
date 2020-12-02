d5ce133f6a9521af58b2bdb8bebd167213d5b059
LeviathanZC <leviathan.zercomp@gmail.com>
2020-12-02 15:47:19 +0300

    Create pages for errors. Replace jsp for new Dirs

--- src/main/java/by/zercomp/application/controller/JspPath.java
+++ src/main/java/by/zercomp/application/controller/JspPath.java
@@ -4,5 +4,16 @@ public class JspPath {
 
     //AUTH
     public static final String SIGN_IN = "/WEB-INF/pages/auth/signIn.jsp";
+    public static final String SIGN_UP = "/WEB-INF/pages/auth/signUp.jsp";
+
+    //ERROR
+    public static final String ERROR_404 = "/WEB-INF/pages/error/error404.jsp";
+    public static final String ERROR_500 = "/WEB-INF/pages/error/error500.jsp";
+
+    //ADMIN
+    public static final String ADMIN_PANEL = "/WEB-INF/pages/admin/adminPanel.jsp";
+    public static final String BLACK_LIST = "/WEB-INF/pages/admin/blackList.jsp";
+    public static final String MANAGEMENT = "/WEB-INF/pages/admin/goodsManagement.jsp";
+
 
 }
